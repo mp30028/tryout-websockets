b4feefea0c0da4ea751159fd68b4ea95eff1dd9a
mp30028 <mp30028@gmail.com>
2025-01-09 06:16:50 +0000

    DbEventMarshal directly publishes mongo events as they come in

--- exhibits/03-websocket-mongo/b-websockets-based/person-api/src/main/java/com/zonesoft/person/api/events/DbEventMarshal.java
+++ exhibits/03-websocket-mongo/b-websockets-based/person-api/src/main/java/com/zonesoft/person/api/events/DbEventMarshal.java
@@ -1,50 +1,52 @@
 package com.zonesoft.person.api.events;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
+import org.springframework.data.mongodb.core.ChangeStreamOptions;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.ChangeStreamOptions.ChangeStreamOptionsBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ReflectionUtils;
 
+import com.mongodb.client.model.changestream.FullDocument;
+
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.FluxSink;
 
 @Component
-public class DbEventMarshal<T> implements ApplicationListener<DbEvent<T>>, Consumer<FluxSink<DbEvent<T>>>{
+public class DbEventMarshal<T> implements Consumer<FluxSink<DbEvent<T>>>{
 	private static final Logger LOGGER = LoggerFactory.getLogger(DbEventMarshal.class);
-    private final BlockingQueue<DbEvent<T>> queue = new LinkedBlockingQueue<>();	
-    private final Executor executor;
-    
+	private final ReactiveMongoTemplate template;
+	
     @Autowired
-	public DbEventMarshal(Executor executor) {
-		this.executor = executor;
+	public DbEventMarshal(ReactiveMongoTemplate template) {
+		this.template = template;
 	}
     
-    
 	@Override
-	public void onApplicationEvent(DbEvent<T> event) {
-		LOGGER.debug("Event recieved by onApplicationEvent Listener. event={}", event);
-		this.queue.offer(event);		
-	}
-
-	@Override
-	public void accept(FluxSink<DbEvent<T>> sink) {
+	public void accept(FluxSink<DbEvent<T>> sink){
 		LOGGER.debug("From accept");
-		this.executor.execute(() -> {
-			while (true)
-				try {
-					DbEvent<T> event = queue.take();
-					sink.next(event);
-				} catch (InterruptedException e) {
-					ReflectionUtils.rethrowRuntimeException(e);
-				}
-			});
+		publishMongoEvents().map(e -> sink.next(e)).subscribe();		
+	}    
+    
+	@SuppressWarnings("unchecked")
+	private Flux<DbEvent<T>> publishMongoEvents() {	
 		
+	    Consumer<ChangeStreamOptionsBuilder> optionsBuilderConsumer = ( (b) -> 
+	    	ChangeStreamOptions.builder()
+            .returnFullDocumentOnUpdate()
+            .returnFullDocumentBeforeChange()
+            .fullDocumentLookup(FullDocument.WHEN_AVAILABLE)
+        );
+	    	    
+		return template
+				.changeStream((Class<T>) Object.class)
+				.withOptions(optionsBuilderConsumer)
+				.watchCollection("persons")
+				.listen()				
+				.map(e -> new DbEventContent<T>(e.getRaw(), (T) e.getBody() ))
+				.map(content -> new DbEvent<T>(content));
 	}
-
 }
--- exhibits/03-websocket-mongo/b-websockets-based/person-api/src/main/java/com/zonesoft/person/api/services/PersonService.java
+++ exhibits/03-websocket-mongo/b-websockets-based/person-api/src/main/java/com/zonesoft/person/api/services/PersonService.java
@@ -1,20 +1,12 @@
 package com.zonesoft.person.api.services;
 
 import java.util.List;
-import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.mongodb.core.ChangeStreamOptions;
-import org.springframework.data.mongodb.core.ChangeStreamOptions.ChangeStreamOptionsBuilder;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.stereotype.Service;
 
-import com.mongodb.client.model.changestream.FullDocument;
 import com.zonesoft.person.api.entities.Person;
-import com.zonesoft.person.api.events.DbEvent;
-import com.zonesoft.person.api.events.DbEventContent;
 import com.zonesoft.person.api.repositories.PersonRepository;
 
 import reactor.core.publisher.Flux;
@@ -26,15 +18,10 @@ public class PersonService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(PersonService.class);
 	
 	private final PersonRepository repository;
-	private final ReactiveMongoTemplate template;
-	private ApplicationEventPublisher  publisher;
 	
 	@Autowired
-	public PersonService(PersonRepository repository, ReactiveMongoTemplate template, ApplicationEventPublisher  publisher) {
+	public PersonService(PersonRepository repository/*, ReactiveMongoTemplate template, ApplicationEventPublisher  publisher*/) {
 		this.repository = repository;
-		this.template = template;
-		this.publisher = publisher;
-		subscribeAndPublishMongoEvents();
 	}
 	
 	public Mono<Person> insert(Person person) {
@@ -79,23 +66,4 @@ public class PersonService {
     	return repository.findByMoniker(moniker);
     }
 	
-	private void subscribeAndPublishMongoEvents() {	
-		
-	    Consumer<ChangeStreamOptionsBuilder> optionsBuilderConsumer = ( (b) -> 
-	    	ChangeStreamOptions.builder()
-            .returnFullDocumentOnUpdate()
-            .returnFullDocumentBeforeChange()
-            .fullDocumentLookup(FullDocument.WHEN_AVAILABLE)
-        );
-	    	    
-		template
-				.changeStream(Person.class)
-				.withOptions(optionsBuilderConsumer)
-				.watchCollection("persons")
-				.listen()				
-				.map(e -> new DbEventContent<Person>(e.getRaw(), e.getBody() ))
-				.map(content -> new DbEvent<Person>(content))
-				.subscribe (event -> this.publisher.publishEvent(event));
-	}
-
 }
