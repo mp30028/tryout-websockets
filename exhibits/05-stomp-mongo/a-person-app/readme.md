27312bd0b99e6793df5baefdd8ffb21163142ffa
mp30028 <mp30028@gmail.com>
2025-01-14 20:22:31 +0000

    seeded 05b

---
+++ exhibits/05-stomp-mongo/a-person-app/readme.md
@@ -0,0 +1,4 @@
+# 05-stomp-mongo/b-person-app
+This version of the app replaces the REST-API completely with a Messaging-API
+
+ 
---
+++ exhibits/05-stomp-mongo/b-person-app/person-api-05b/src/test/java/com/zonesoft/person/PersonApiAppTest.java
@@ -0,0 +1,13 @@
+package com.zonesoft.person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiAppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
