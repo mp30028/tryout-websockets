86b30b311bdac0cc16c5e3cd3470114b1436fcc9
mp30028 <mp30028@gmail.com>
2025-01-16 19:19:55 +0000

    messaging api completed together with UI to interact with it

--- exhibits/05-stomp-mongo/b-person-app/person-api-05b/src/main/java/com/zonesoft/person/api/controllers/MessageController.java
+++ exhibits/05-stomp-mongo/b-person-app/person-api-05b/src/main/java/com/zonesoft/person/api/controllers/MessageController.java
@@ -1,7 +1,7 @@
 package com.zonesoft.person.api.controllers;
 
+import java.util.List;
 import java.util.function.Consumer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +19,6 @@ import com.zonesoft.person.api.events.DbEvent;
 import com.zonesoft.person.api.events.DbEventContent;
 import com.zonesoft.person.api.services.PersonService;
 
-//import reactor.core.publisher.Mono;
-
 @Controller
 public class MessageController<T>{
 	private static final Logger LOGGER = LoggerFactory.getLogger(MessageController.class);
@@ -34,9 +32,10 @@ public class MessageController<T>{
 		this.mongoTemplate = mongoTemplate;
 		this.messagingTemplate = messagingTemplate;
 		this.service = service;
-		publishMongoEvents();		
+		publishMongoEvents();
+		
 	}
-           
+    
 	@SuppressWarnings("unchecked")
 	private void publishMongoEvents() {	
 		
@@ -55,95 +54,47 @@ public class MessageController<T>{
 				.map(e -> new DbEventContent<T>(e.getRaw(), (T) e.getBody() ))
 				.map(content -> new DbEvent<T>(content))
 				.subscribe(event -> {
-					LOGGER.debug("converting and publishing to \"/topic/db-event\" the db-event {}", event);
-					messagingTemplate.convertAndSend("/topic/db-event", event);
+					LOGGER.debug("converting and publishing to \"/topic/person/events\" the db-event {}", event);					
+					messagingTemplate.convertAndSend("/topic/person/events", event);
 				});
-	}
-    
+	};
+	
+	@MessageMapping("/person/get-all")
+	@SendTo("/topic/person/get-all")
+    public List<Person> getAll(){
+    	return service.findAll().collectList().as((m)->m.block());
+    }	
+	
+	@MessageMapping("/person/get-by-id")
+	@SendTo("/topic/person/get-by-id")
+    public List<Person> getById(List<String> listOfIds){
+		LOGGER.debug("listOfIds={}", listOfIds);
+    	return service.findByListOfIds(listOfIds).collectList().as((m)->m.block());
+    }		
+	
+	@MessageMapping("/person/get-by-moniker")
+	@SendTo("/topic/person/get-by-moniker")
+    public List<Person> getByMoniker(String moniker){
+		LOGGER.debug("moniker={}", moniker);
+    	return service.findByMoniker(moniker).collectList().as((m)->m.block());
+    }
+	
 	@MessageMapping("/person/insert")
 	@SendTo("/topic/person/insert")
     public Person insert( Person person){
-    	return service.insert(person).as((m)-> m.block());        
+    	return service.insert(person).as((m)-> m.block());
     }
-        
-//    @GetMapping
-//    public Mono<ResponseEntity<List<Person>>> findAll(){
-//    	Flux<Person> personFlux = service.findAll();
-//    	if (Objects.nonNull(personFlux)) {
-//	        return personFlux
-//	        	.collectList()
-//	        	.map( l -> {
-//	        		if (Objects.nonNull(l) && (l.size()>0)) {
-//	        			return ResponseEntity.ok().body(l);	
-//	        		}else {
-//	        			return ResponseEntity.noContent().build();
-//	        		}
-//	        	});
-//	    }else {
-//	    	return Mono.just(ResponseEntity.noContent().build());
-//	    }
-//    }
-//
-//    @GetMapping(params = {"id"})
-//    public Mono<ResponseEntity<List<Person>>> findByListOfIds(@RequestParam List<String> id){
-//    	Flux<Person> personFlux = service.findByListOfIds(id);
-//    	if (Objects.nonNull(personFlux)) {
-//	        return personFlux
-//	        	.collectList()
-//	        	.map( l -> {
-//	        		if (Objects.nonNull(l) && (l.size()>0)) {
-//	        			return ResponseEntity.ok().body(l);	
-//	        		}else {
-//	        			return ResponseEntity.noContent().build();
-//	        		}
-//	        	});
-//	    }else {
-//	    	return Mono.just(ResponseEntity.noContent().build());
-//	    }
-//    }
-//
-//    @GetMapping("/{id}")
-//    public Mono<ResponseEntity<Person>> findById(@PathVariable String id){
-//        Mono<Person> personMono = service.findById(id);
-//        return personMono.map(ResponseEntity::ok)
-//                .defaultIfEmpty(ResponseEntity.notFound().build());
-//    }
-//
-//    @GetMapping(params = {"moniker"})
-//    public Mono<ResponseEntity<List<Person>>> findByMoniker(@RequestParam String moniker){
-//    	Flux<Person> personFlux = service.findByMoniker(moniker);
-//    	if (Objects.nonNull(personFlux)) {
-//	        return personFlux
-//	        	.collectList()
-//	        	.map( l -> {
-//	        		if (Objects.nonNull(l) && (l.size()>0)) {
-//	        			return ResponseEntity.ok().body(l);	
-//	        		}else {
-//	        			return ResponseEntity.noContent().build();
-//	        		}
-//	        	});
-//	    }else {
-//	    	return Mono.just(ResponseEntity.noContent().build());
-//	    }
-//    	
-//    }
-//    
-//    @PutMapping("/{id}")
-//    public Mono<ResponseEntity<Person>> update(@PathVariable String id, @RequestBody Person person){
-//    	LOGGER.debug("FROM ApiController.update: id={}, person={}", id, person);
-//    	person.setId(id);
-//        return service.update(person)
-//        		.map(updated -> {
-//        			LOGGER.debug("FROM ApiController.update: updatedPerson={}", updated);
-//        			return updated;
-//        		})
-//                .map( p -> ResponseEntity.accepted().body(p));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public Mono<ResponseEntity<Void>> deleteById(@PathVariable String id){
-//        return service.deleteById(id)
-//                .map( r -> ResponseEntity.accepted().<Void>build());
-//    }	
+
+	@MessageMapping("/person/update")
+	@SendTo("/topic/person/update")
+    public Person update( Person person){
+    	return service.update(person).as((m)-> m.block());
+    }	
 	
+	@MessageMapping("/person/delete-by-id")
+	@SendTo("/topic/person/delete-by-id")
+    public String deleteById(String id){
+    	return service.deleteById(id).thenReturn(id).as((m)-> m.block());		
+    }
+
 }
