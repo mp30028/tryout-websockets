874ce4b21c69aa3fa6a5484588b1cc02b13c8421
mp30028 <mp30028@gmail.com>
2025-01-29 16:44:19 +0000

    recreated experimental instance

--- exhibits/06-secure-stomp/z-experimental/persons-gateway/src/main/java/com/zonesoft/persons/gateway/config/SpringCloudConfig.java
+++ exhibits/06-secure-stomp/z-experimental/persons-gateway/src/main/java/com/zonesoft/persons/gateway/config/SpringCloudConfig.java
@@ -1,28 +1,13 @@
 package com.zonesoft.persons.gateway.config;
 
-import org.springframework.http.HttpHeaders;
-
-import java.util.Set;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-
-import reactor.core.publisher.Mono;
 
 @Configuration
 public class SpringCloudConfig {
 
-	
-	
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
@@ -46,29 +31,4 @@ public class SpringCloudConfig {
         		.route("default-route", r -> r.path("/**").filters(f->f.tokenRelay()).uri("https://localhost:7075"))
                 .build();
     } 
-}
-
-@Component
-@Order(0)
-class LoggingFilter implements GlobalFilter {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
-    
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-       
-    	LOGGER.debug("Pre Filter executed");
-       
-        String requestPath = exchange.getRequest().getPath().toString();
-        LOGGER.debug("Request path = " + requestPath);
-        
-        HttpHeaders headers = exchange.getRequest().getHeaders();
-        Set<String> headerNames = headers.keySet();
- 
-        headerNames.forEach((header) -> {
-        	LOGGER.debug(header + " " + headers.get(header));
-        });    	
-    	
-        return chain.filter(exchange);
-    }
- 
 }
